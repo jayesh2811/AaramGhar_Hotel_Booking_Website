b6f0dc9be0abd615f81650ef97bd6e9f23e7f2c2
Jayesh Patil <patiljayesh95666@gmail.com>
2025-01-22 16:04:44 +0530

    map feature

--- models/room.js
+++ models/room.js
@@ -1,58 +1,59 @@
-const { timeStamp } = require('console');
-const mongoose = require('mongoose');
-const { type } = require('os');
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+const { type } = require("os");
 
-const roomSchema = mongoose.Schema({
+const roomSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     maxCount: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     phoneNumber: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     rentPerDay: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
 
     imgUrls: [],
 
-
     currBookings: [],
 
     type: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     amenities: [],
 
     location: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     address: {
-        type: String,
-        required: true
-    }
-
-} , {
+      type: String,
+      required: true,
+    },
+  },
+  {
     timeStamp: true,
-});
+  }
+);
 
 const RoomModel = mongoose.model("rooms", roomSchema);
 
